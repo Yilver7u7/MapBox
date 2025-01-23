2e5df3651e28a78b49c198b7e98662cc64a245c4
Yilver7u7 <yilversgg@gmail.com>
2025-01-23 15:51:02 -0500

    Learning standAlone with Fernando

---
+++ src/app/alone/component/counter-alone/counter-alone.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'counter-alone',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter-alone.component.html',
+  styleUrl: './counter-alone.component.css'
+})
+export class CounterAloneComponent {
+  counter: number = 0;
+
+  decrement(){
+    this.counter -= 1;
+  }
+
+  increment(){
+    this.counter += 1;
+  }
+
+  reset(){
+    this.counter = 0;
+  }
+
+}
